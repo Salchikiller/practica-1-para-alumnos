b57a457425a6a7adf08d209309e0a1935f271772
Salchikiller <caralepenafiel@gmail.com>
2024-02-19 22:36:25 +0100

    javadoc addded

--- src/mates/Matematicas.java
+++ src/mates/Matematicas.java
@@ -1,24 +1,27 @@
 package mates;
+
 // importar generador de números aleatorios desde java .uitl libarys
+
 import java.util.Random;
 
+public class Matematicas {
 
-public class Matematicas{
     /**
-     * * Genera una aproximación al número Pi mediante el método de
-     * * Montecarlo. El parámetro `pasos` indica el número de puntos
-     * * generado.
-     * */
-
+     * Genera una aproximación al número Pi mediante el método de Montecarlo.
+     * El parámetro `pasos` indica el número de puntos generado.
+     *
+     * @param pasos El número de pasos para generar puntos.
+     * @return Una aproximación del número Pi.
+     */
     public static double generarNumeroPiIterativo(long pasos){
 
-    // init Radom instancia
-         Random rand = new Random(); 
+        // init Radom instancia
+        Random rand = new Random(); 
 
-    // crear contador para realizar un seguimiento de los puntos internos
+        // crear contador para realizar un seguimiento de los puntos internos
         int points_inside_circle = 0;
+
         // crear bucle para crear tantas coordenadas x e y aleatorias como lo soliciten los "pasos" 
-        
         for (int i = 0; i < pasos; i++) {
             double x = rand.nextDouble();
             double y = rand.nextDouble();
@@ -26,16 +29,13 @@ public class Matematicas{
             //valida si xey están dentro o fuera de las bases en los cálculos de monte carlo
             if (x * x + y * y <= 1){
                 points_inside_circle ++;
-
             }
+        }
 
+        // calcula el pastel aproximado multiplicando por el resto de los cuadrantes
+        double aprox_pi= 4.0*((double)points_inside_circle/pasos);
 
-         }
-         // calcula el pastel aproximado multiplicando por el resto de los cuadrantes
-         double aprox_pi= 4.0*(points_inside_circle/pasos);
-
-    
-         // devuelve el valor de o pastel aporx
+        // devuelve el valor de pi aporxcimado
         return aprox_pi;
     }
 }
