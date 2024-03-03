0dab446cacfd108ab8e51ad644d4d39dc6ca414f
Salchikiller <caralepenafiel@gmail.com>
2024-03-03 22:11:10 +0100

    working code but has stackoverflow probelms with bigger numbers aroun 5500

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -5,6 +5,6 @@ import mates.Matematicas;
 public class Principal{
     public static void main(String[] args){
         System.out.println("El número PI es " +
-                Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0])));
+                Matematicas.generarPiRecursivamente(Integer.parseInt(args[0])));
     }
 }
